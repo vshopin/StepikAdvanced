2d8dae060712b0f4281853ed7b8becafedc2515f
shopi <shopin_va@mail.ru>
2023-06-10 22:28:38 +0700

    Created task with set

---
+++ Sets/Timur.py
@@ -0,0 +1,6 @@
+"""
+
+"""
+# Создаем список чисел
+n, m, k, x, y, z = int(input()), int(input()), int(input()), int(input()), int(input()), int(input())
+print((n - x) + (m - x - y) + (k - y) + x + y + z)
